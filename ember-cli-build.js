cc588483947022eea9f6afe396073ae929e4454c
Kyle Mellander <kylemellander@gmail.com>
2015-10-02 13:47:56 -0700

    Add semantic files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    lessOptions: {
+      paths: [
+        'bower_components/semantic-ui'
+      ]
+    },
+    SemanticUI: {
+      css: false,
+      javascript: true,
+      fonts: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
